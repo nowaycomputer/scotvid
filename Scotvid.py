c8d58d58c3ac14e1b0208bc0d745d989d89d7372
Chris Wallace <chris@Chriss-Mac-mini.local>
2021-12-14 14:25:42 +0000

    fixed links

--- Scotvid.py
+++ Scotvid.py
@@ -181,8 +181,9 @@ with st.spinner('Grabbing latest data...'):
 
 st.subheader('Notes')
 st.success("""
--   Data is updated each weekday at 2pm. Very hacky/basic code available at [https://github.com/nowaycomputer/scotvid](https://github.com/nowaycomputer/scotvid)
-\n\n-    Latest merged [national data](https://github.com/nowaycomputer/scotvid/blob/main/merged_data.csv) and [local data](https://github.com/nowaycomputer/scotvid/blob/main/df_city.csv) as .csv
+-   Data is updated each weekday at 2pm. 
+\n\n-    Very hacky/basic code available at [https://github.com/nowaycomputer/scotvid](https://github.com/nowaycomputer/scotvid)
+\n\n-    Latest merged [national data](https://github.com/nowaycomputer/scotvid/blob/main/data/merged_data.csv) and [local data](https://github.com/nowaycomputer/scotvid/blob/main/data/df_city.csv) as .csv
 \n\n-    All test/case data is by sample date
 \n\n-    Hospitalisation and ICU rates are rough estimates calculated using the ratio of hospital and ICU admissions 7 and 14 days respectively after the cases by sample date
 \n\n-    Not all data is synchronised to the same date and the most recent day isn't shown because it is always significantly incomplete
